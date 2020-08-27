aa7cdc1164d781216d6e294b44f975f2522ea75e
khalid almuhawis <khalidalmuhawis0@gmail.com>
2020-08-27 16:41:11 +0300

    Views & Templates

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,2 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
+def home(request):
+    context = {
+    "msg": "Hello World!",
+    "name": "Khalid",
+    "task": "02"
+    }
+
+    return render(request, "home.html", context)
